5ee619c49de2169ab1d2d9ce96df9d04324e4688
cetin <cetin.tekin@alo-tech.com>
2018-12-01 12:05:32 +0300

    36 hpcp features are added

---
+++ hpcpWriteDB2.py
@@ -0,0 +1,63 @@
+import numpy as np
+import sqlite3
+import essentia
+import essentia.standard as es
+
+db_conn = sqlite3.connect("data.db")
+db_cursor = db_conn.cursor()
+
+
+names = ['blues','classical','country','disco','hiphop','jazz','metal','pop','reggae','rock']
+
+
+for name in names:
+
+    for i in range(10):
+        song_name = name + '.' + '0000' + str(i) + '.wav'
+        print(song_name)
+        # Compute all features, aggregate only 'mean' and 'stdev' statistics for all low-level, rhythm and tonal frame features
+        features, features_frames = es.MusicExtractor(lowlevelStats=['mean', 'stdev'],
+                                                      rhythmStats=['mean', 'stdev'],
+                                                      tonalStats=['mean', 'stdev'])("genres_new/"+name+"/"+song_name)
+
+
+
+        song_name = name + '.' + '0000' + str(i) + '.au'
+        hpcp_vector = features["tonal.hpcp.mean"]
+
+        for i in range(1,37):
+            sql = "UPDATE songs SET avg_hpcp_" + str(i) + "=? WHERE song_name=?"
+            db_cursor.execute(sql,(float(hpcp_vector[i-1]),song_name))
+            db_conn.commit()
+
+
+
+
+
+
+
+
+    for i in range(10,100):
+        song_name = name + '.' + '000' + str(i) + '.wav'
+        print(song_name)
+        # Compute all features, aggregate only 'mean' and 'stdev' statistics for all low-level, rhythm and tonal frame features
+        features, features_frames = es.MusicExtractor(lowlevelStats=['mean', 'stdev'],
+                                                      rhythmStats=['mean', 'stdev'],
+                                                      tonalStats=['mean', 'stdev'])("genres_new/"+name+"/"+song_name)
+
+
+
+
+        song_name = name + '.' + '000' + str(i) + '.au'
+        hpcp_vector = features["tonal.hpcp.mean"]
+
+        for i in range(1,37):
+            sql = "UPDATE songs SET avg_hpcp_" + str(i) + "=? WHERE song_name=?"
+            db_cursor.execute(sql,(float(hpcp_vector[i-1]),song_name))
+            db_conn.commit()
+
+
+
+
+
+db_conn.close()
